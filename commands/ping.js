9874e82a1889a99e2aa9e575f5b3e39c89a6a034
erdem <erdemhso@mail.ru>
2020-11-24 09:48:02 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "ping",
   aliases: ["gecikme"],
-  execute(client, msg, args){
+  async execute(client, msg, args){
         const zaman = Date.now();
         const gonder = await client.createMessage(msg.channel.id, `Ölçülüyor..`);
         return gonder.edit(`Pong! \`${Date.now() - zaman}\`ms`);
